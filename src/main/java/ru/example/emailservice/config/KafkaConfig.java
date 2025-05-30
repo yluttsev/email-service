abaec38e48def2a61f51ba1785b1427b41465042
Yuri Luttsev <floomyz@vk.com>
2025-05-31 00:15:23 +0300

    Добавлена конфигурация для кафки

--- src/main/java/ru/example/emailservice/config/KafkaConfig.java
+++ src/main/java/ru/example/emailservice/config/KafkaConfig.java
@@ -1,7 +1,9 @@
 package ru.example.emailservice.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 
 @EnableKafka
+@Configuration
 public class KafkaConfig {
 }
