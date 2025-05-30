f48352dfa851025f988d1ab765ac088ae6586385
Yuri Luttsev <floomyz@vk.com>
2025-05-30 17:31:35 +0300

    Исправлен тест запуска контекста

--- src/test/java/ru/example/emailservice/EmailServiceApplicationTests.java
+++ src/test/java/ru/example/emailservice/EmailServiceApplicationTests.java
@@ -3,7 +3,13 @@ package ru.example.emailservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+        "spring.mail.host=test",
+        "spring.mail.port=465",
+        "spring.mail.username=user",
+        "spring.mail.password=password",
+        "spring.kafka.bootstrap-servers=localhost:9092"
+})
 class EmailServiceApplicationTests {
 
     @Test
