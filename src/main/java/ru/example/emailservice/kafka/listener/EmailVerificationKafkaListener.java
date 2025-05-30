2bcf73bc3966033dd77b4be708ee7421483e4faf
Yuri Luttsev <floomyz@vk.com>
2025-05-30 18:03:42 +0300

    Добавлена ручаная десериализация события kafka

--- src/main/java/ru/example/emailservice/kafka/listener/EmailVerificationKafkaListener.java
+++ src/main/java/ru/example/emailservice/kafka/listener/EmailVerificationKafkaListener.java
@@ -1,6 +1,9 @@
 package ru.example.emailservice.kafka.listener;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
@@ -9,14 +12,21 @@ import ru.example.emailservice.service.EmailSenderService;
 
 import java.util.Map;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class EmailVerificationKafkaListener {
 
     private final EmailSenderService emailSenderService;
+    private final ObjectMapper objectMapper;
 
     @KafkaListener(topics = "${topic.email-verification}")
-    public void receiveEmailVerificationEvent(@Payload EmailVerificationEvent event) {
-        emailSenderService.sendEmail(event.email(), "email-verification", Map.of("code", event.code()));
+    public void receiveEmailVerificationEvent(@Payload String event) {
+        try {
+            EmailVerificationEvent emailVerificationEvent = objectMapper.readValue(event, EmailVerificationEvent.class);
+            emailSenderService.sendEmail(emailVerificationEvent.email(), "email-verification", Map.of("code", emailVerificationEvent.code()));
+        } catch (JsonProcessingException e) {
+            log.error("Ошибка преобразования JSON: {}", e.getMessage());
+        }
     }
 }
