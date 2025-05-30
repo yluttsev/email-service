759de49dbdf811e1cdc5887eb79bfd94f4eb625f
Yuri Luttsev <floomyz@vk.com>
2025-05-30 17:38:38 +0300

    Исправлен тест запуска контекста

--- src/test/java/ru/example/emailservice/EmailServiceApplicationTests.java
+++ src/test/java/ru/example/emailservice/EmailServiceApplicationTests.java
@@ -8,7 +8,10 @@ import org.springframework.boot.test.context.SpringBootTest;
         "spring.mail.port=465",
         "spring.mail.username=user",
         "spring.mail.password=password",
-        "spring.kafka.bootstrap-servers=localhost:9092"
+        "spring.kafka.bootstrap-servers=localhost:9092",
+        "eureka.instance.hostname=app",
+        "eureka.instance.non-secure-port=8080",
+        "eureka.client.service-url.defaultZone=localhost:8761"
 })
 class EmailServiceApplicationTests {
 
